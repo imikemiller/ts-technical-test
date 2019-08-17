d3bc4805eb7760c4ac1291f04cec85460461ed40
Mike Miller <mike@mikemiller.uk>
2019-08-17 09:30:09 +0200

    Component skeleton, installed mui, storybook

---
+++ src/ArtworkSearch/components/ArtworkSearch.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TextField } from '@material-ui/core'
+
+const ArtworkSearch = () => {
+    return (
+        <h1>text</h1>
+    );
+};
+
+export default ArtworkSearch;
---
+++ src/ArtworkSearch/components/ArtworkSearch.stories.js
@@ -0,0 +1,5 @@
+import { React } from 'react';
+import { storiesOf } from '@storybook/react';
+import ArtworkSearchComponent from './ArtworkSearch.component';
+
+storiesOf('Artworks', module).add('Search', () => <ArtworkSearchComponent />)
